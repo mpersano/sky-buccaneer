deaa839b90747b1d1f01f7cd519b7cc8a18f2136
Mauro Persano <mpersano.dev@gmail.com>
2020-07-26 22:18:42 -0300

    Handle basic window events

--- src/main.cc
+++ src/main.cc
@@ -9,25 +9,26 @@
 
 namespace {
 
-class Window : private NonCopyable
+class GameWindow : private NonCopyable
 {
 public:
-    Window(int width, int height, const char *title);
-    ~Window();
+    GameWindow(int width, int height, const char *title);
+    ~GameWindow();
 
-    int width() const { return m_width; }
-    int height() const { return m_height; }
-    operator GLFWwindow *() const { return m_window; }
+    void renderLoop();
 
 private:
-    int m_width;
-    int m_height;
-    GLFWwindow *m_window;
+    static void sizeCallback(GLFWwindow *window, int width, int height);
+    static void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mode);
+
+    void resize(int width, int height);
+    void key(int key, int scancode, int action, int mode);
+
+    GLFWwindow *m_window = nullptr;
+    std::unique_ptr<World> m_world;
 };
 
-Window::Window(int width, int height, const char *title)
-    : m_width(width)
-    , m_height(height)
+GameWindow::GameWindow(int width, int height, const char *title)
 {
     glfwInit();
     glfwSetErrorCallback([](int error, const char *description) {
@@ -39,23 +40,21 @@ Window::Window(int width, int height, const char *title)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     m_window = glfwCreateWindow(width, height, title, nullptr, nullptr);
 
+    glfwSetWindowUserPointer(m_window, this);
+    glfwSetKeyCallback(m_window, GameWindow::keyCallback);
+    glfwSetWindowSizeCallback(m_window, GameWindow::sizeCallback);
+
     glfwMakeContextCurrent(m_window);
     glfwSwapInterval(1);
-}
 
-Window::~Window()
-{
-    glfwDestroyWindow(m_window);
-    glfwTerminate();
-}
+    auto error = glewInit();
+    if (error != GLEW_OK) {
+        panic("Failed to initialize GLEW: %s\n", glewGetErrorString(error));
+    }
 
-}
-
-int main()
-{
-    Window window(800, 400, "hello");
-
-    glewInit();
+    if (!glewIsSupported("GL_VERSION_4_3")) {
+        panic("OpenGL 4.3 not supported\n");
+    }
 
     glEnable(GL_DEBUG_OUTPUT);
     glDebugMessageCallback(
@@ -63,27 +62,65 @@ int main()
                const void * /*user*/) { std::cerr << source << ':' << type << ':' << severity << ':' << message << '\n'; },
             nullptr);
 
-    glfwSetKeyCallback(window, [](GLFWwindow *window, int key, int scancode, int action, int mode) {
-        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, GL_TRUE);
-    });
+    m_world.reset(new World);
 
-    {
-        World world;
-        world.resize(window.width(), window.height());
+    int windowWidth, windowHeight;
+    glfwGetWindowSize(m_window, &windowWidth, &windowHeight);
+    m_world->resize(windowWidth, windowHeight);
+}
 
-        double curTime = glfwGetTime();
-        while (!glfwWindowShouldClose(window)) {
-            float elapsed;
-            auto now = glfwGetTime();
-            elapsed = now - curTime;
-            curTime = now;
+void GameWindow::renderLoop()
+{
+    double curTime = glfwGetTime();
+    while (!glfwWindowShouldClose(m_window)) {
+        float elapsed;
+        auto now = glfwGetTime();
+        elapsed = now - curTime;
+        curTime = now;
 
-            world.update(elapsed);
-            world.render();
+        m_world->update(elapsed);
+        m_world->render();
 
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-        }
+        glfwSwapBuffers(m_window);
+        glfwPollEvents();
     }
+}
+
+GameWindow::~GameWindow()
+{
+    m_world.reset();
+    glfwDestroyWindow(m_window);
+    glfwTerminate();
+}
+
+void GameWindow::keyCallback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+    auto *gameWindow = reinterpret_cast<GameWindow *>(glfwGetWindowUserPointer(window));
+    gameWindow->key(key, scancode, action, mode);
+}
+
+void GameWindow::sizeCallback(GLFWwindow *window, int width, int height)
+{
+    auto *gameWindow = reinterpret_cast<GameWindow *>(glfwGetWindowUserPointer(window));
+    gameWindow->resize(width, height);
+}
+
+void GameWindow::key(int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(m_window, 1);
+    }
+}
+
+void GameWindow::resize(int width, int height)
+{
+    m_world->resize(width, height);
+}
+
+} // namespace
+
+int main()
+{
+    GameWindow window(800, 400, "hello");
+    window.renderLoop();
 }
