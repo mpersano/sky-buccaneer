3981f8b82262cf5209376303049a68f0ab8dc242
Mauro Persano <mpersano.dev@gmail.com>
2021-05-01 18:31:03 -0300

    Remove dead code

--- src/texture.cc
+++ src/texture.cc
@@ -3,23 +3,6 @@
 
 #include <memory>
 
-namespace {
-
-template<typename T>
-static T
-nextPowerOf2(T n)
-{
-    --n;
-    n |= n >> 1;
-    n |= n >> 2;
-    n |= n >> 4;
-    n |= n >> 8;
-    n |= n >> 16;
-    return n + 1;
-}
-
-} // namespace
-
 namespace GL {
 
 Texture::Texture()
