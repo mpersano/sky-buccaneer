cb45d55d447ad15a1a1cb8f99f0fa8b800f9c7ec
Mauro Persano <mpersano.dev@gmail.com>
2021-05-04 20:25:17 -0300

    Remove wrong assert
    Channels always has # of channels in input file, even when we force 4
    channels.

--- src/image.cc
+++ src/image.cc
@@ -13,7 +13,6 @@ bool Image::load(const std::string &path)
     unsigned char *data = stbi_load(path.c_str(), &m_width, &m_height, &channels, 4);
     if (!data)
         return false;
-    assert(channels == 4);
     const auto *pixels = reinterpret_cast<const uint32_t *>(data);
     m_bits.assign(pixels, pixels + m_width * m_height);
     stbi_image_free(data);
